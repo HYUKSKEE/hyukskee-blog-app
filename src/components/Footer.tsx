50b50d19d0673cd45fd28926bf2dcea7df5a4c20
hyukskee <gin280833@gmail.com>
2023-11-28 12:16:30 +0900

    add: dark테마 기능 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,50 @@
 import styled from "styled-components";
+import { BsSun, BsMoonFill } from "react-icons/bs";
+import { useContext } from "react";
+import { ThemeContext } from "context/ThemeContext";
 
 export default function Footer() {
+  const context = useContext(ThemeContext);
   return (
-    <>
-      <CommonFooter>Hyukskee blog</CommonFooter>
-    </>
+    <CommonFooter>
+      <FooterText>Hyukskee blog</FooterText>
+      <div>
+        {context.theme === "light" ? (
+          <BsSunIcon onClick={context.toggleMode} />
+        ) : (
+          <BsMoonFillIcon onClick={context.toggleMode} />
+        )}
+      </div>
+    </CommonFooter>
   );
 }
 
 const CommonFooter = styled.footer`
   position: fixed;
   bottom: 0;
-  width: 100%;
+  right: 0;
+  left: 0;
   min-height: 100px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 20px 0;
+  padding: 20px 100px;
+  border-top: 1px solid #dedede;
+  background-color: #ffffff;
+`;
+
+const FooterText = styled.div`
   font-size: 24px;
   font-weight: 900;
-  border-top: 1px solid #dedede;
   color: #dfdfdf;
-  background-color: #ffffff;
+`;
+
+const BsSunIcon = styled(BsSun)`
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+const BsMoonFillIcon = styled(BsMoonFill)`
+  font-size: 24px;
+  cursor: pointer;
 `;
